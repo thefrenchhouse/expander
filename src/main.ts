78318d10e6d5227b14dbfeba9ab0de3fbccd8224
Charly POLY <cpoly55@gmail.com>
2016-11-12 15:22:19 +0100

    add Spotify Web API integration example

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
-function helloWorld(name: string): String {
-  return `Hello, ${name}`;
-}
+var SpotifyWebApi = require('spotify-web-api-node'),
+    config        = require('../../application-config.json');
 
-export default function greeter(name) { // tslint:disable-line
-  return helloWorld(name);
-}
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId : config.SpotifyClientId,
+  clientSecret : config.SpotifyClientSecret,
+  redirectUri : config.SpotifyRedirectUri
+});
+
+spotifyApi.setAccessToken(config.SpotifyAccessToken);
+
+spotifyApi.getMySavedTracks({
+  limit : 2,
+  offset: 1
+})
+.then(function(data) {
+  console.log('Done!', data);
+}, function(err) {
+  console.log('Something went wrong!', err);
+});
